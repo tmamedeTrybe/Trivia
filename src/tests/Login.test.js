6ccb544fdb3d59718436a42a4111a546b672555b
Daniel Saraiva <danieldsvn@gmail.com>
2022-05-25 12:43:49 -0300

    Req 4 ok. 100% coverage. fix: instala @testing-library/dom para userEvent funcionar

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,10 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import App from '../App.js'
-import userEvent from '@testing-library/user-event';
 
+// const INITIAL_STATE = {
+//   player: '',
+//   assertions: '',
+//   score: '',
+//   gravatarEmail: '',
+// };
 
 describe('Testa a tela de login', () => {
   it('Tem 2 inputs e 2 botões na tela', () => {
@@ -22,7 +27,7 @@ describe('Testa a tela de login', () => {
     const playButton = screen.getByRole('button', {name: /play/i});
     expect(playButton).toHaveAttribute('disabled');
   })
-  it('Jogador pode escrever nome e email', () => {
+  it('Jogador pode escrever nome e email e clicar em "Play"', () => {
     renderWithRouterAndRedux(<App/>)
 
     const nameInput = screen.getByRole('textbox', {name: /nome/i});
@@ -31,34 +36,47 @@ describe('Testa a tela de login', () => {
     const emailInput = screen.getByRole('textbox', {name: /email/i});
     expect(emailInput).toBeInTheDocument();
     
-    fireEvent.change(nameInput, {target: {value: 'daniel'}})
-    // userEvent.type(nameInput, 'daniel');
+    userEvent.type(nameInput, 'daniel');
     expect(nameInput).toHaveValue('daniel');
 
-    fireEvent.change(emailInput, {target: {value: 'daniel@trybe.com'}})
-    // userEvent.type(emailInput, 'daniel');
-    expect(emailInput).toHaveValue('daniel@trybe.com');
-    it('Informações do usuário são enviados ao localStorage ao clicar em Play', () => {
-      renderWithRouterAndRedux(<App/>)
-
-    const nameInput = screen.getByRole('textbox', {name: /nome/i});
-    expect(nameInput).toBeInTheDocument();
-    
-    const emailInput = screen.getByRole('textbox', {name: /email/i});
-    expect(emailInput).toBeInTheDocument();
-    
-    fireEvent.change(nameInput, {target: {value: 'daniel'}})
-    // userEvent.type(nameInput, 'daniel');
-    expect(nameInput).toHaveValue('daniel');
-
-    fireEvent.change(emailInput, {target: {value: 'daniel@trybe.com'}})
-    // userEvent.type(emailInput, 'daniel');
+    userEvent.type(emailInput, 'daniel@trybe.com');
     expect(emailInput).toHaveValue('daniel@trybe.com');
 
     const playButton = screen.getByRole('button', {name: /play/i});
-    // userEvent.click(playButton)
-    fireEvent.click(playButton);
-    })
+    expect(playButton).toBeInTheDocument();
+
+    userEvent.click(playButton);
   })
+    // it('chave do usuário é enviado ao localStorage ao clicar em Play', () => {
+    //   // const setItem = await jest.fn()
+
+    //   const { history } = renderWithRouterAndRedux(<App/>, INITIAL_STATE, '/');
+
+    // const nameInput = screen.getByRole('textbox', {name: /nome/i});
+    // expect(nameInput).toBeInTheDocument();
+    
+    // const emailInput = screen.getByRole('textbox', {name: /email/i});
+    // expect(emailInput).toBeInTheDocument();
+    
+    // userEvent.type(nameInput, 'daniel');
+    // expect(nameInput).toHaveValue('daniel');
+
+    
+    // userEvent.type(emailInput, 'daniel@trybe.com');
+    // expect(emailInput).toHaveValue('daniel@trybe.com');
+
+    // const playButton = screen.getByRole('button', {name: /play/i});
+    // expect(playButton).toBeInTheDocument();
+
+    // userEvent.click(playButton);
+
+    // // history.push({ location: '/game'});
+    // // // // expect(localStorage).toHaveBeenCalled();
+    
+    // // // // renderWithRouterAndRedux(<App/>, null, '/game');
+    
+    // // // //https://stackoverflow.com/questions/21418580/what-is-the-difference-between-before-and-beforeeach
+    // // expect(window.location.pathname).toBe('/game');
+    // }) 
   
 })
